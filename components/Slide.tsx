28785630249180876930b37b99e3a3c2e231e14b
alexjackhughes <alex@alexjackhughes.com>
2021-03-24 09:24:57 +0000

    fixed on mobile

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -23,7 +23,7 @@ export default function Slide() {
   return (
     <main className="flex flex-row w-full">
       <PreviousIcon onClick={previousSlide} />
-      <div className="flex flex-col items-center justify-center flex-1 text-left w-full px-5 md:px-32 lg:px-64">
+      <div className="flex flex-col items-center justify-center text-left w-full px-5 md:px-32 lg:px-64">
         <Title>{primary}</Title>
         <Title isFancy>{secondary}</Title>
       </div>
@@ -38,7 +38,7 @@ interface Props {
 
 const Title: React.FC<Props> = ({ isFancy, children }) => (
   <h1
-    className={`text-5xl md:text-7xl lg:text-8xl font-bold text-left w-full select-none text-gray-100 py-0 md:py-2 ${
+    className={`break-all text-3xl md:text-7xl lg:text-8xl font-bold text-left w-full select-none text-gray-100 py-0 pb-2 md:py-2 ${
       isFancy && "skittles"
     }`}
   >
